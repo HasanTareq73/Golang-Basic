e63dff47ae1227f18c73aa2f72e816fd6913f708
HasanTareq73 <hasantareq73@gmail.com>
2022-09-27 03:50:15 +0600

    First Commit

---
+++ HelloWorld.go
@@ -0,0 +1,23 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Hello, World!")
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello Mahdi")
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, This is my first program using Golang")
+}
